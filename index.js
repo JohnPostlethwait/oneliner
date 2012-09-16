acc544ad7cd760a3265ed82cbd256e688f2f1c73
John Postlethwait <john.postlethwait@nebula.com>
2012-09-16 02:40:25 -0700

    fixing return

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 
-module.exports = function () {
-  var oneline = function (string, dont_trim) {
-    // Remove the newline characters, in their various formats.
-    var onelined = string.replace(/(\r\n|\n|\r)/gm, '');
+module.exports = function (string, dont_trim) {
+  // Remove the newline characters, in their various formats.
+  var onelined = string.replace(/(\r\n|\n|\r)/gm, '');
 
-    if (dont_trim) { return onelined; }
+  if (dont_trim) { return onelined; }
 
-    // Trim the extra whitespace on both ends of the string.
-    var trimmed = onelined.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+  // Trim the extra whitespace on both ends of the string.
+  var trimmed = onelined.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 
-    return trimmed;
-  };
-
-  return oneline;
+  return trimmed;
 };
