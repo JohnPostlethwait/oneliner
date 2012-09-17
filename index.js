2e300ef3cf613230fdeaf94c5cf24d59c16c4717
John Postlethwait <john.postlethwait@nebula.com>
2012-09-17 00:33:25 -0700

    Better regex for onelineing html

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 
 module.exports = function (string) {
-  // Trim the extra whitespace on both ends of the string.
-  string = string.replace(/(^\s\s*|\s\s*$)/g, '');
-  string = string.replace(/((\r\n|\n|\r) +| +(\r\n|\n|\r))/, ' ');
-
-  // Remove the newline characters, in their various formats.
-  string = string.replace(/(\r\n|\n|\r)/gm, '');
+  // Remove the newline characters, in their various formats, and erroneous extra spaces.
+  string = string.replace(/(\s\s+|\r\n|\n|\r)/gm, '');
 
   return string;
 };
